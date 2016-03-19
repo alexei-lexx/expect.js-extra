a5800aba67dde6123917dfc17cc9a28585dc0853
Alexei L <alexei.lexx@gmail.com>
2016-03-19 14:11:43 +0200

    add containEql() to support deep equality

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('expect.js');
+var expect = require('expect.js');
+
+require('./lib/contain_eql')(expect);
+
+
+module.exports = expect;
---
+++ test/contain_eql.js
@@ -0,0 +1,92 @@
+describe('expect.js-extra', function() {
+  var expect = require('../index');
+
+  function err(fn, msg) {
+    try {
+      fn();
+      throw new Error('Expected an error');
+    } catch (err) {
+      expect(msg).to.be(err.message);
+    }
+  }
+
+  describe('expect(array).to.containEql(element)', function() {
+    context('when the element from the string array is given', function() {
+      var arr = ['foo', 'bar'];
+      var el = 'foo';
+
+      it('succeeds', function() {
+        expect(arr).to.containEql(el);
+      });
+    });
+
+    context('when the element from the number array is given', function() {
+      var arr = [1, 2];
+      var el = 2;
+
+      it('succeeds', function() {
+        expect(arr).to.containEql(el);
+      });
+    });
+
+    context('when the element from the object array is given', function() {
+      var arr = [{ a: 1, b: 2 }, { a: 3, b: 4 }];
+      var el = { a: 1, b: 2 };
+
+      it('succeeds', function() {
+        expect(arr).to.containEql(el);
+      });
+    });
+
+    context('when the element doesn\'t belong to the array', function() {
+      var arr = ['foo'];
+      var el = 'bar';
+
+      it('raises the appropriate message', function() {
+        err(function() {
+          expect(arr).to.containEql(el);
+        }, 'expected [ \'foo\' ] to contain \'bar\'');
+      });
+    });
+  });
+
+  describe('expect(array).to.not.containEql(element)', function() {
+    context('when the string doesn\'t belong to the string array', function() {
+      var arr = ['foo', 'bar'];
+      var el = 'baz';
+
+      it('succeeds', function() {
+        expect(arr).to.not.containEql(el);
+      });
+    });
+
+    context('when the number doesn\'t belong to the number array', function() {
+      var arr = [2, 3];
+      var el = 1;
+
+      it('succeeds', function() {
+        expect(arr).to.not.containEql(el);
+      });
+    });
+
+    context('when the object doesn\'t belong to the object array', function() {
+      var arr = [{ a: 1, b: 2 }, { a: 3, b: 4 }];
+      var el = { a: 5, b: 6 };
+
+      it('succeeds', function() {
+        expect(arr).to.not.containEql(el);
+      });
+    });
+
+    context('when the element belongs to the array', function() {
+      var arr = ['bar', 'foo'];
+      var el = 'foo';
+
+      it('raises the appropriate message', function() {
+        err(function() {
+          expect(arr).to.not.containEql(el);
+        }, 'expected [ \'bar\', \'foo\' ] to not contain \'foo\'');
+      });
+    });
+  });
+});
